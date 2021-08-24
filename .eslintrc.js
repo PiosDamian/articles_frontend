4a747dfe728aa91f6f69db68af6f390e6b0e02ac
Damian Pioś <damian.pios@infinite.pl>
2021-08-24 21:15:45 +0200

    set up

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,8 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "comma-dangle": ["error", "never"],
+    "quotes": ["error", "single", { "avoidEscape": false }]
   },
   overrides: [
     {
